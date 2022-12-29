485e7382394e8dcf7fcf99cd0abd30fa2181d63a
Krzysztof Samordak <109137913+Krzysztof-Samordak@users.noreply.github.com>
2022-12-29 10:24:06 +0100

    Add files via upload
    Project for Keysight EL Electronic Load control.

---
+++ PE_MB Tester/Keysight.KtEL30000/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿/// <copyright>3Shape A/S</copyright>
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ThreeShapeKtEL30000")]
+[assembly: AssemblyDescription("KtEL30000 connection and test console app")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("3Shape")]
+[assembly: AssemblyProduct("ThreeShapeKtEL30000")]
+[assembly: AssemblyCopyright("Copyright 2022 3Shape A/S")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
